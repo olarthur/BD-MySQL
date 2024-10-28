73309a66db1ff0097d76e04a37eb0b0525f14e24
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-10-28 16:55:46 -0300

    Update Cadastro.sql

--- Cadastro.sql
+++ Cadastro.sql
@@ -23,7 +23,16 @@ CREATE TABLE `cursos` (
 
 LOCK TABLES `cursos` WRITE;
 
-INSERT INTO `cursos` VALUES (1,'HTML4','Curso de HTML5',40,37,2014),(2,'Algoritimos','Lógica de Programaçao',20,15,2014),(3,'Photoshop','Dicas de Photoshop CC',10,8,2014),(4,'PGP','Curso de PHP para iniciantes',40,20,2010),(5,'Jarva','Introdução a linguagem Java',10,29,2000),(6,'MySQL','Banco de Dados MySQL',30,15,2016),(7,'Word','Curso completo de Word',40,30,2016),(8,'Sapateado','Dança Ritmica',40,30,2018),(9,'Cozinha Árabe','Aprenda a fazer Kibe',40,30,2018),(10,'Youtube','Ganhar inscritos',5,2,2018);
+INSERT INTO `cursos` VALUES (1,'HTML4','Curso de HTML5',40,37,2014),
+(2,'Algoritimos','Lógica de Programaçao',20,15,2014),
+(3,'Photoshop','Dicas de Photoshop CC',10,8,2014),
+(4,'PGP','Curso de PHP para iniciantes',40,20,2010),
+(5,'Jarva','Introdução a linguagem Java',10,29,2000),
+(6,'MySQL','Banco de Dados MySQL',30,15,2016),
+(7,'Word','Curso completo de Word',40,30,2016),
+(8,'Sapateado','Dança Ritmica',40,30,2018),
+(9,'Cozinha Árabe','Aprenda a fazer Kibe',40,30,2018),
+(10,'Youtube','Ganhar inscritos',5,2,2018);
 
 UNLOCK TABLES;
 
