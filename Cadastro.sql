b21da6e1c539fcab5148bb8534c2a4a087d7a396
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-10-28 16:49:42 -0300

    Update Cadastro.sql

--- Cadastro.sql
+++ Cadastro.sql
@@ -1,29 +1,11 @@
-CREATE DATABASE  IF NOT EXISTS `cadastro` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE  IF NOT EXISTS `cadastro`;
 USE `cadastro`;
--- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
---
--- Host: localhost    Database: cadastro
--- ------------------------------------------------------
--- Server version	8.0.40
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `cursos`
 --
 
 DROP TABLE IF EXISTS `cursos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `cursos` (
   `idcurso` int NOT NULL,
   `nome` varchar(30) NOT NULL,
@@ -34,16 +16,15 @@ CREATE TABLE `cursos` (
   PRIMARY KEY (`idcurso`),
   UNIQUE KEY `nome` (`nome`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `cursos`
 --
 
 LOCK TABLES `cursos` WRITE;
-/*!40000 ALTER TABLE `cursos` DISABLE KEYS */;
+
 INSERT INTO `cursos` VALUES (1,'HTML4','Curso de HTML5',40,37,2014),(2,'Algoritimos','Lógica de Programaçao',20,15,2014),(3,'Photoshop','Dicas de Photoshop CC',10,8,2014),(4,'PGP','Curso de PHP para iniciantes',40,20,2010),(5,'Jarva','Introdução a linguagem Java',10,29,2000),(6,'MySQL','Banco de Dados MySQL',30,15,2016),(7,'Word','Curso completo de Word',40,30,2016),(8,'Sapateado','Dança Ritmica',40,30,2018),(9,'Cozinha Árabe','Aprenda a fazer Kibe',40,30,2018),(10,'Youtube','Ganhar inscritos',5,2,2018);
-/*!40000 ALTER TABLE `cursos` ENABLE KEYS */;
+
 UNLOCK TABLES;
 
 --
@@ -51,8 +32,7 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `meliantes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `meliantes` (
   `codigo` int DEFAULT NULL,
   `id` int NOT NULL AUTO_INCREMENT,
@@ -65,25 +45,14 @@ CREATE TABLE `meliantes` (
   `nacionalidade` varchar(20) DEFAULT 'Brasil',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `meliantes`
 --
 
 LOCK TABLES `meliantes` WRITE;
-/*!40000 ALTER TABLE `meliantes` DISABLE KEYS */;
+
 INSERT INTO `meliantes` VALUES (NULL,1,'Bauman',NULL,'2004-01-10','M',75.50,1.76,'Brasil'),(NULL,2,'Platão',NULL,'1904-01-10','M',85.30,1.86,'Greece'),(NULL,3,'Kafka',NULL,'1804-01-10','M',79.70,1.81,'Brasil'),(NULL,4,'Aristoteles',NULL,'1704-01-10','M',81.90,1.85,'Germany'),(NULL,8,'Alighieri',NULL,'1604-01-10','M',78.20,1.73,'Italy'),(NULL,9,'Shakespeare',NULL,'1650-01-10','M',73.20,1.77,'England'),(NULL,10,'Homero',NULL,'1683-01-10','M',87.00,1.79,'France');
-/*!40000 ALTER TABLE `meliantes` ENABLE KEYS */;
+
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-10-28 16:34:45
